b4d1715fce58772819f27a2e3f524bedef70009b
Omar yousry <omarimr64@gmail.com>
2024-08-10 10:46:35 +0300

    Adding info section

--- js/main.js
+++ js/main.js
@@ -1,19 +1,30 @@
 "use strict";
 
+// Navbar menu functionality
+const navbar = document.querySelector("header nav");
 const openMenu = document.querySelector(".bars");
 const menu = document.querySelector("header nav ul");
 const closeMenu = document.querySelector(".close");
 
-openMenu.addEventListener("click", () => {
+const startMenu = function () {
   menu.classList.add("active");
-});
-
-closeMenu.addEventListener("click", () => {
+  document.body.classList.add("stop-scroll");
+};
+const endMenu = function () {
   menu.classList.remove("active");
-});
+  document.body.classList.remove("stop-scroll");
+};
 
+openMenu.addEventListener("click", startMenu);
+closeMenu.addEventListener("click", endMenu);
 document.addEventListener("keydown", (e) => {
   if (e.key === "Escape") {
-    menu.classList.remove("active");
+    endMenu();
   }
 });
+
+// Make the navigation bar sticky
+const addSticky = function () {
+  navbar.classList.toggle("sticky", window.scrollY > 0);
+};
+window.addEventListener("scroll", addSticky);
